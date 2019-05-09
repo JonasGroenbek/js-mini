86668408b74eb8d87c2d1099a412f78520ccb0cd
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-05-09 11:27:49 +0200

    working webform authentication

--- src/controllers/authenticate.ts
+++ src/controllers/authenticate.ts
@@ -1,6 +1,10 @@
 import express, {Request, Response} from "express";
 import {ApplicationError, attempt} from "../errors/error";
 import bodyParser from "body-parser";
+import {sessionStore} from "../util/formHelpers";
+import {authenticationProvider} from "../util/configuration";
+import {sessionAuthentication} from "../auth/authenticationMiddleware";
+import {IncorrectCredentialsError} from "../auth/authentication";
 
 const router = express.Router();
 
@@ -10,14 +14,33 @@ router.use(bodyParser.urlencoded({extended: true}));
 // @ts-ignore
 router.get("/", async function (req: Request, res: Response, next: (err: ApplicationError) => any) {
     await attempt(next, function () {
-        res.render("authenticate");
+        res.render("authenticate", {
+            formErrors: sessionStore(req).getErrors()
+        });
     });
 });
 
 // Accepts form information sent by the authentication page.
 // @ts-ignore
 router.post("/", async function (req: Request, res: Response, next: (err: ApplicationError) => any) {
-    res.send(JSON.stringify(req.body));
+    await attempt(next, async function () {
+
+        const errors = sessionStore(req);
+        try {
+            const result = await authenticationProvider.authenticate(req.body.email, req.body.password);
+            sessionAuthentication(req).setAuthenticatedUser(result);
+            res.redirect("/");
+            return;
+        } catch (e) {
+            if (e instanceof IncorrectCredentialsError) {
+                errors.pushError("form", "Could not authenticate using provided credentials.");
+                res.redirect("authenticate");
+                return;
+            }
+
+            throw e;
+        }
+    });
 });
 
 export default router;
