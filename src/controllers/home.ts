59be673f4945a5f7d27e289f382f69c6fa202cc6
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-05-09 11:32:08 +0200

    display authentication status

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,9 +1,12 @@
 import {Request, Response} from "express";
 import {ApplicationError, attempt} from "../errors/error";
+import {sessionAuthentication} from "../auth/authenticationMiddleware";
 
 // Renders the home page.
 export default async function (req: Request, res: Response, next: (err: ApplicationError) => any) {
     await attempt(next, function () {
-        res.render("home");
+        res.render("home", {
+            authenticatedUser: sessionAuthentication(req).getAuthenticatedUser()
+        });
     });
 }
